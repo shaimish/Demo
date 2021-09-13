0037f485808451f645fface1452e514b41d673f0
shaimish <shaimish@gmail.com>
2021-09-13 23:26:57 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!  add new change
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
