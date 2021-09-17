2c4443d8e8fbdebe99f0fe7412b487dfe447a9df
shaimish <shaimish@gmail.com>
2021-09-17 16:34:30 +0530

    added PMD Fidbug and check style in POM and a bug

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        if (args[1] == args[0]){
+            System.out.println( "this is a bug.....!" );
+        }
     }
 }
